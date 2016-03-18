710ea5840ecbd1dd1093fca4ec222c4d3d217fac
houjiazong <houjiazong@gmail.com>
2016-03-18 18:40:45 +0800

    add lib

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 export Bar from './Bar';
 export Doughnut from './Doughnut.js';
-export HorizontalBar from './HorizontalBar.js';
 export Line from './Line.js';
 export Pie from './Pie.js';
 export PolarArea from './PolarArea.js';
