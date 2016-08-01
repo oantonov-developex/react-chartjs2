4d096e2ceef9f6b83f5af2d28db7bd32118aaf30
Oleg Antonov <oleg.antonov@developex.org>
2016-08-01 13:16:15 +0300

    Added horizontal bar chart

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ class Core extends Component {
 Core.propTypes = {
   data: PropTypes.object.isRequired,
   options: PropTypes.object,
-  type: PropTypes.oneOf(['doughnut', 'pie', 'line', 'bar', 'radar', 'polarArea', 'bubble']).isRequired
+  type: PropTypes.oneOf(['doughnut', 'pie', 'line', 'bar', 'radar', 'polarArea', 'bubble', 'horizontalBar']).isRequired
 };
 
 export default Core;
